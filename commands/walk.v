928fa8fa315eff9942e6ea736a9c7b339249aaf7
daniel-le97 <107774403+daniel-le97@users.noreply.github.com>
2024-05-20 18:13:38 -0600

    make utils.walker to reuse dir walking fn

--- commands/walk.v
+++ commands/walk.v
@@ -3,7 +3,7 @@ module commands
 import cli
 import os
 import utils
-import sync.pool
+// import sync.pool
 
 const walk_path = cli.Flag{
 	name: 'path'
@@ -13,7 +13,7 @@ const walk_path = cli.Flag{
 	default_value: [os.getwd() + '/']
 }
 
-// walk reads the initial directory and walks all subdirectories for parrallel processing
+// walk reads the initial directory and walks all subdirectories for parr
 pub const walk = cli.Command{
 	name: 'walk'
 	description: 'walks a directory recursively (testing)'
@@ -26,30 +26,7 @@ pub const walk = cli.Command{
 	}
 	execute: fn (cmd cli.Command) ! {
 		dir := cmd.flags.get_string('path')!
-		all_files := utils.new_array()
-
-		mut pp := pool.new_pool_processor(
-			callback: fn [all_files] (mut pp pool.PoolProcessor, idx int, wid int) bool {
-				item := pp.get_item[string](idx)
-				os.walk(item, fn [all_files] (filename string) {
-					all_files(filename)
-				})
-				return true
-			}
-		)
-		mut dirs := []string{}
-		paths := os.ls(dir) or { panic(err) }
-		for path in paths {
-			full_path := dir + path
-			if os.is_dir(full_path) {
-				dirs << full_path
-			} else {
-				all_files(full_path)
-			}
-		}
-		println('walking ${dirs.len} directories...')
-		pp.work_on_items(dirs)
-		println('all_files: ${all_files('')}')
-		println('all_files: ${all_files('').len}')
+		files := utils.walker(dir)
+		println('files: ${files.len}')
 	}
 }
---
+++ utils/walker.v
@@ -0,0 +1,29 @@
+module utils
+
+import sync.pool
+import os
+
+pub fn walker(p string) []string {
+	arr := new_array()
+	mut pp := pool.new_pool_processor(
+		callback: fn [arr] (mut pp pool.PoolProcessor, idx int, wid int) bool {
+			item := pp.get_item[string](idx)
+			os.walk(item, fn [arr] (filename string) {
+				arr(filename)
+			})
+			return true
+		}
+	)
+	paths := os.ls(p) or { panic(err) }
+	mut dirs := []string{}
+	for path in paths {
+		full_path := p + path
+		if os.is_dir(full_path) {
+			dirs << full_path
+		} else {
+			arr(full_path)
+		}
+	}
+	pp.work_on_items(dirs)
+	return arr('')
+}
